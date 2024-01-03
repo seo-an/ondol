558855984b6de67bd3f2a1bb9e153504ce38a82d
seoan <jay2019lee@gmail.com>
2024-01-03 23:12:58 +0900

    lazy 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/layout/Layout.js';
 import Home from './components/routes/Home.js';
-import OpenApiPage from './components/routes/OpenApiPage.js';
-import RestApiPage from './components/routes/RestApiPage.js';
-import About from './components/routes/About.js';
+// import OpenApiPage from './components/routes/OpenApiPage.js';
+// import RestApiPage from './components/routes/RestApiPage.js';
+// import About from './components/routes/About.js';
 
-import WindowPopup from './components/component/WindowPopup.js';
+// import WindowPopup from './components/component/WindowPopup.js';
 
-import GuestBookWrite from './components/component/guestbook/GuestBookWrite.js';
-import GuestBookEdit from './components/component/guestbook/GuestBookEdit.js';
-import MyCalendar from './components/component/cards/calendar/MyCalendar.js';
-import ModalExample from './components/component/cards/modalExample.js';
-import LayerExample from './components/component/cards/layerPopupExample.js';
-import WindowPopExample from './components/component/cards/windowPopupExample.js';
-import InfiniteScrollExample from './components/component/cards/infiniteScrollExample.js';
-import PapagoTransExample from './components/component/cards/papagoExample.js';
+// import GuestBookWrite from './components/component/guestbook/GuestBookWrite.js';
+// import GuestBookEdit from './components/component/guestbook/GuestBookEdit.js';
+// import MyCalendar from './components/component/cards/calendar/MyCalendar.js';
+// import ModalExample from './components/component/cards/modalExample.js';
+// import LayerExample from './components/component/cards/layerPopupExample.js';
+// import WindowPopExample from './components/component/cards/windowPopupExample.js';
+// import InfiniteScrollExample from './components/component/cards/infiniteScrollExample.js';
+// import PapagoTransExample from './components/component/cards/papagoExample.js';
+
+const OpenApiPage = lazy(() => import('./components/routes/OpenApiPage.js'));
+const RestApiPage = lazy(() => import('./components/routes/RestApiPage.js'));
+const About = lazy(() => import('./components/routes/About.js'));
+
+const WindowPopup = lazy(() => import('./components/component/WindowPopup.js'));
+
+const GuestBookWrite = lazy(() => import('./components/component/guestbook/GuestBookWrite.js'));
+const GuestBookEdit = lazy(() => import('./components/component/guestbook/GuestBookEdit.js'));
+const MyCalendar = lazy(() => import('./components/component/cards/calendar/MyCalendar.js'));
+const ModalExample = lazy(() => import('./components/component/cards/modalExample.js'));
+const LayerExample = lazy(() => import('./components/component/cards/layerPopupExample.js'));
+const WindowPopExample = lazy(() => import('./components/component/cards/windowPopupExample.js'));
+const InfiniteScrollExample = lazy(() => import('./components/component/cards/infiniteScrollExample.js'));
+const PapagoTransExample = lazy(() => import('./components/component/cards/papagoExample.js'));
+
 
 const App: React.FC = () => {
 
